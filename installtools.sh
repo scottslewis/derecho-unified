1649b0119275fddea18d9714a8d4f6e056daa440
slewis <slewis@composent.com>
2018-04-26 16:55:25 -0700

    Added clone of derecho-unified, install of openjdk-8-jdk, swig, and build of derecho-unified

--- installtools.sh
+++ installtools.sh
@@ -5,3 +5,16 @@ sudo apt-get -y update
 
 # CMake
 sudo apt-get -y install cmake
+
+sudo apt-get -y install openjdk-8-jdk
+
+sudo apt-get -y install swig
+
+cd ~
+
+git clone --recursive https://github.com/Derecho-Project/derecho-unified.git
+
+mkdir Release
+cd Release
+cmake -DCMAKE_BUILD_TYPE=Release ..
+make
